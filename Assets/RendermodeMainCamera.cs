ed97a6b9887ff6419bd215205f130640ddd4a074
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-12-07 15:52:47 -0800

    New Waterline (more better)

---
+++ Assets/RendermodeMainCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RendermodeMainCamera : MonoBehaviour
+{
+    public Canvas canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.worldCamera = Camera.main;
+    }
+
+}
