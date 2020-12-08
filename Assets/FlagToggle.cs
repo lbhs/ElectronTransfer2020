be42dde398cc1c3ff0448831e21f5d3ed77f30ae
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-12-07 17:03:57 -0800

    Flag toggle

---
+++ Assets/FlagToggle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagToggle : MonoBehaviour
+{
+    
+    public void toggleTheThimg(){
+        GameObject.Find("Lable Canvas").GetComponent<LableManager>().ToggleFlags();
+    }
+}
--- Assets/LableManager.cs
+++ Assets/LableManager.cs
@@ -7,4 +7,15 @@ public class LableManager : MonoBehaviour
     public GameObject[] imagePrefabs;
     public GameObject TextPrefab;
     public GameObject FlagPrefab;
+    [ContextMenu("test")]
+    public void ToggleFlags()
+    {
+        foreach (Transform item in transform)
+        {
+            if (item.name == "Flag(Clone)")
+            {
+                item.gameObject.SetActive(!item.gameObject.activeSelf);
+            }
+        }
+    }
 }
