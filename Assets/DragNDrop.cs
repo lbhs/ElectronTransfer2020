60f1204ed25eb09467f3727e6e79d8962f88fa5c
Benjamin Keller <Benjaminkeller@gmail.com>
2021-04-18 14:20:29 -0700

    removed wild card code, it can now be safely deleted from all scenes

--- Assets/DragNDrop.cs
+++ Assets/DragNDrop.cs
@@ -8,16 +8,16 @@ public class DragNDrop : MonoBehaviour
     public bool CanBeDragged = true;
     private Vector3 mOffset;
     private float mZCoord;
-    private GameObject rightMenu;
-    private Vector3 menuOffest;
-    private Ray ray;
-    private RaycastHit hit;
-    private GameObject rightCanvas;
+    //private GameObject rightMenu;
+   // private Vector3 menuOffest;
+   // private Ray ray;
+    //private RaycastHit hit;
+   // private GameObject rightCanvas;
     private Vector3 mouseStartPos;
     private Vector3 mouseEndPos;
     private float mouseStartTime;
     private float mouseEndTime;
-    public bool canBeRightClicked = true;
+   // public bool canBeRightClicked = true;
 
     void OnMouseDown()
     {
@@ -28,7 +28,7 @@ public class DragNDrop : MonoBehaviour
             // Store offset = gameobject world pos - mouse world pos
             // offset allows you to grab the object from the side of the circle, not just the center
             mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
-            rightCanvas.GetComponent<RightClickHelper>().HideRightMenu();
+            //rightCanvas.GetComponent<RightClickHelper>().HideRightMenu();
             //starting position for flicking sphere to increase velocity
             Vector3 mousePos = Input.mousePosition * -1;
             mousePos.z = 0;
@@ -187,87 +187,87 @@ public class DragNDrop : MonoBehaviour
     {
         gameObject.GetComponent<Rigidbody>().MovePosition(gameObject.transform.position);
         //defining objects/varibles
-        rightCanvas = GameObject.Find("Right-Click Canvas");
-        rightMenu = rightCanvas.GetComponent<RightClickHelper>().rightMenu;
-        rightMenu.SetActive(false);
+        //rightCanvas = GameObject.Find("Right-Click Canvas");
+        //rightMenu = rightCanvas.GetComponent<RightClickHelper>().rightMenu;
+       // rightMenu.SetActive(false);
         mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
     }
 
-    //updates everyframe
-    void Update()
-    {
-        //set up for finding the object that was right clicked
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    ////updates everyframe
+    //void Update()
+    //{
+    //    //set up for finding the object that was right clicked
+    //    ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        //like OnMouseDown, but for right-click
-        if (Input.GetMouseButtonDown(1))
-        {
-            //find out if an object was hit 
-            if (Physics.Raycast(ray, out hit) && canBeRightClicked)
-            {
+    //    //like OnMouseDown, but for right-click
+    //    if (Input.GetMouseButtonDown(1))
+    //    {
+    //        //find out if an object was hit 
+    //        if (Physics.Raycast(ray, out hit) && canBeRightClicked)
+    //        {
 
-                //caculates where the menu must be
-                rightCanvas.GetComponent<RightClickHelper>().CheckRightVisablity();
+    //            //caculates where the menu must be
+    //            rightCanvas.GetComponent<RightClickHelper>().CheckRightVisablity();
 
-                //------------------------sets the position of the right-click menu------------------------
-                //rightMenu.transform.position = Input.mousePosition + menuOffest;
-                //makes sure that right click menu is in screen
-
-
-                //makes menu visable
-                rightMenu.SetActive(true);
-
-                //update the varible so that RightClickHelper knows what sphere to edit
-                rightCanvas.GetComponent<RightClickHelper>().currentSphere = hit.rigidbody.gameObject;
-
-                //------------------------updates menu vaules on click------------------------
-
-                //mass
-                rightCanvas.GetComponent<RightClickHelper>().Mass.GetComponent<InputField>().text = hit.rigidbody.mass.ToString();
-
-                //charge
-                rightCanvas.GetComponent<RightClickHelper>().Charge.GetComponent<InputField>().text = hit.rigidbody.gameObject.GetComponent<charger>().charge.ToString();
-
-                //color
-                if (hit.rigidbody.gameObject.GetComponent<Renderer>().material.color == Color.red)
-                {
-                    rightCanvas.GetComponent<RightClickHelper>().Color1.GetComponent<Toggle>().isOn = true;
-                    rightCanvas.GetComponent<RightClickHelper>().toggleGroup.GetComponent<ToggleGroup>().NotifyToggleOn(rightCanvas.GetComponent<RightClickHelper>().Color1.GetComponent<Toggle>());
-                }
-                else
-                if (hit.rigidbody.gameObject.GetComponent<Renderer>().material.color == Color.blue)
-                {
-                    rightCanvas.GetComponent<RightClickHelper>().Color2.GetComponent<Toggle>().isOn = true;
-                    rightCanvas.GetComponent<RightClickHelper>().toggleGroup.GetComponent<ToggleGroup>().NotifyToggleOn(rightCanvas.GetComponent<RightClickHelper>().Color2.GetComponent<Toggle>());
-                }
-                else
-                if (hit.rigidbody.gameObject.GetComponent<Renderer>().material.color == Color.green)
-                {
-                    rightCanvas.GetComponent<RightClickHelper>().Color3.GetComponent<Toggle>().isOn = true;
-                    rightCanvas.GetComponent<RightClickHelper>().toggleGroup.GetComponent<ToggleGroup>().NotifyToggleOn(rightCanvas.GetComponent<RightClickHelper>().Color3.GetComponent<Toggle>());
-                }
-                else
-                if (hit.rigidbody.gameObject.GetComponent<Renderer>().material.color == Color.yellow)
-                {
-                    rightCanvas.GetComponent<RightClickHelper>().Color4.GetComponent<Toggle>().isOn = true;
-                    rightCanvas.GetComponent<RightClickHelper>().toggleGroup.GetComponent<ToggleGroup>().NotifyToggleOn(rightCanvas.GetComponent<RightClickHelper>().Color4.GetComponent<Toggle>());
-                }
-
-                //size
-                if (hit.rigidbody.gameObject.transform.localScale == new Vector3(1, 1, 1))
-                {
-                    rightCanvas.GetComponent<RightClickHelper>().Size1.GetComponent<Toggle>().isOn = true;
-                }
-                else
-                {
-                    rightCanvas.GetComponent<RightClickHelper>().Size2.GetComponent<Toggle>().isOn = true;
-                }
-
-            }
-        }
+    //            //------------------------sets the position of the right-click menu------------------------
+    //            //rightMenu.transform.position = Input.mousePosition + menuOffest;
+    //            //makes sure that right click menu is in screen
 
 
-    }
+    //            //makes menu visable
+    //            rightMenu.SetActive(true);
+
+    //            //update the varible so that RightClickHelper knows what sphere to edit
+    //            rightCanvas.GetComponent<RightClickHelper>().currentSphere = hit.rigidbody.gameObject;
+
+    //            //------------------------updates menu vaules on click------------------------
+
+    //            //mass
+    //            rightCanvas.GetComponent<RightClickHelper>().Mass.GetComponent<InputField>().text = hit.rigidbody.mass.ToString();
+
+    //            //charge
+    //            rightCanvas.GetComponent<RightClickHelper>().Charge.GetComponent<InputField>().text = hit.rigidbody.gameObject.GetComponent<charger>().charge.ToString();
+
+    //            //color
+    //            if (hit.rigidbody.gameObject.GetComponent<Renderer>().material.color == Color.red)
+    //            {
+    //                rightCanvas.GetComponent<RightClickHelper>().Color1.GetComponent<Toggle>().isOn = true;
+    //                rightCanvas.GetComponent<RightClickHelper>().toggleGroup.GetComponent<ToggleGroup>().NotifyToggleOn(rightCanvas.GetComponent<RightClickHelper>().Color1.GetComponent<Toggle>());
+    //            }
+    //            else
+    //            if (hit.rigidbody.gameObject.GetComponent<Renderer>().material.color == Color.blue)
+    //            {
+    //                rightCanvas.GetComponent<RightClickHelper>().Color2.GetComponent<Toggle>().isOn = true;
+    //                rightCanvas.GetComponent<RightClickHelper>().toggleGroup.GetComponent<ToggleGroup>().NotifyToggleOn(rightCanvas.GetComponent<RightClickHelper>().Color2.GetComponent<Toggle>());
+    //            }
+    //            else
+    //            if (hit.rigidbody.gameObject.GetComponent<Renderer>().material.color == Color.green)
+    //            {
+    //                rightCanvas.GetComponent<RightClickHelper>().Color3.GetComponent<Toggle>().isOn = true;
+    //                rightCanvas.GetComponent<RightClickHelper>().toggleGroup.GetComponent<ToggleGroup>().NotifyToggleOn(rightCanvas.GetComponent<RightClickHelper>().Color3.GetComponent<Toggle>());
+    //            }
+    //            else
+    //            if (hit.rigidbody.gameObject.GetComponent<Renderer>().material.color == Color.yellow)
+    //            {
+    //                rightCanvas.GetComponent<RightClickHelper>().Color4.GetComponent<Toggle>().isOn = true;
+    //                rightCanvas.GetComponent<RightClickHelper>().toggleGroup.GetComponent<ToggleGroup>().NotifyToggleOn(rightCanvas.GetComponent<RightClickHelper>().Color4.GetComponent<Toggle>());
+    //            }
+
+    //            //size
+    //            if (hit.rigidbody.gameObject.transform.localScale == new Vector3(1, 1, 1))
+    //            {
+    //                rightCanvas.GetComponent<RightClickHelper>().Size1.GetComponent<Toggle>().isOn = true;
+    //            }
+    //            else
+    //            {
+    //                rightCanvas.GetComponent<RightClickHelper>().Size2.GetComponent<Toggle>().isOn = true;
+    //            }
+
+    //        }
+    //    }
+
+
+    //}
     private bool CheckConstraints(Rigidbody rb)
     {
         bool itWorks;
